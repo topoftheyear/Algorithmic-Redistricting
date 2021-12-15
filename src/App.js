392b94b4ca1199b254c9d959ee5d868845a85da1
Cinnamon Toast <topoftheyear@Gmail.com>
2021-12-14 20:52:18 -0600

    Successfully got image dimensions passed from temp image to main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 
-import MagImage from './MagImage';
+//import MagImage from './MagImage';
 import TrashImage from './TrashImage';
 import importAll from './getImages';
 
@@ -15,23 +15,38 @@ class App extends React.Component {
 			percent: '0.98',
 			smallSize: {width: 200, height: 200},
 			bigSize: {width: 2000, height: 2000},
-			currentlySelected: this.images['AL_districts_0.98.png'],
+			currentlySelectedBig: this.images['AL_districts_0.98.png'],
+			currentlySelectedSmall: this.images['AL_districts_0.98_small.png'],
 		}
 		this.updateSelection();
 	}
 
 	updateSelection() {
 		this.setState({
-			currentlySelected: this.images[this.state.state + '_districts_' + this.state.percent + '.png'],
+			currentlySelectedBig: this.images[this.state.state + '_districts_' + this.state.percent + '.png'],
+			currentlySelectedSmall: this.images[this.state.state + '_districts_' + this.state.percent + '_small.png'],
 		});
 	}
+
+	smallDimUpdate = dim => {
+		this.setState({
+			smallSize: dim
+		});
+		console.log(dim);
+	};
+
+	bigDimUpdate = dim => {
+		this.setState({
+			bigSize: dim
+		});
+	};
 
 	render() {
 		return(
 			<div className="App">
 				<header className="App-header">
-					<TrashImage selection={this.state.currentlySelected}/>
-				</header>
+					<TrashImage selection={this.state.currentlySelectedSmall} updateDim={this.smallDimUpdate} />
+					<TrashImage selection={this.state.currentlySelectedBig} updateDim={this.bigDimUpdate} />				</header>
 			</div>
 		);
 	}
--- src/TrashImage.js
+++ src/TrashImage.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 
-class TrashImage extends React.Component {
-	onImgLoad({target:img}) {
-		const dim = {height:img.naturalHeight,
-					width:img.naturalWidth};
-		console.log(dim);
+class TrashImage extends React.Component {	
+	onImgLoad = ({target:img}) => {
+		console.log(this.props);
+		this.props.updateDim({
+			width: img.naturalWidth,
+			height: img.naturalHeight,
+		});
 	}
 
 	render() {
-		console.log(this.props.selection)
+		console.log(this.props);
 		return(
 			<div>
 				<img onLoad={this.onImgLoad} src={this.props.selection.default} alt="trash"/>
