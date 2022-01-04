aa15ec43f111edd5873e6730d21f0350a9f79116
Cinnamon Toast <topoftheyear@Gmail.com>
2022-01-04 17:16:18 -0600

    Yet more meta tag business

--- src/App.js
+++ src/App.js
@@ -142,16 +142,16 @@ class App extends React.Component {
 				<MetaTags>
 					<title>Algorithmic Redistricting</title>
 					<meta name='description' content='Each U.S. state redistricted by algorithm using Census data and zip codes.' />
-					<meta name='keywords' content='Redistrict, Algorithm, Gerrymandering' />
 					<meta name='author' content='Riley Conlin' />
 					<meta property='og:url' content='https://topoftheyear.github.io/Algorithmic-Redistricting/' />
 					<meta property='og:title' content='Algorithmic Redistricting' />
+					<meta property='og:type' content='website' />
 					<meta property='og:description' content='Each U.S. state redistricted by algorithm using Census data and zip codes.' />
 					<meta property='og:image' content='./results/images/AL_districts_0.98.png' />
-					<meta property='twitter:card' content='summary' />
-					<meta property='twitter:title' content='Algorithmic Redistricting' />
-					<meta property='twitter:description' content='Each U.S. state redistricted by algorithm using Census data and zip codes.' />
-					<meta property='twitter:image' content='./results/images/AL_districts_0.98.png' />
+					<meta name='twitter:card' content='summary' />
+					<meta name='twitter:title' content='Algorithmic Redistricting' />
+					<meta name='twitter:description' content='Each U.S. state redistricted by algorithm using Census data and zip codes.' />
+					<meta name='twitter:image' content='./results/images/AL_districts_0.98.png' />
 				</MetaTags>
 				<header className='App-header'>
 					<h4>Algorithmic Redistricting</h4>
